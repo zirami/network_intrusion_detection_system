dceda4b9038f31f313249ef2a4a8ceff1b11ebef
Nguyễn Thành Thắng <76993858+zirami@users.noreply.github.com>
2022-12-04 01:38:22 +0700

    submit

--- run-swarm.sh
+++ run-swarm.sh
@@ -13,11 +13,11 @@ KAFKA_TOPIC="netflows"
 NUM_OF_SPARK_WORKERS="2"
 MODELS_LOCATION="s3://thangntsbucket/Models/"
 TRAINING_FILE_LOCATION="s3://thangntsbucket/Datasets/NSL-KDD/KDDTrain+.txt"
-SPARK_TRAIN_ARGUMENTS="--conf spark.hadoop.fs.s3a.endpoint=s3.us-east-1.amazonaws.com --conf spark.hadoop.fs.s3a.access.key=abc --conf spark.hadoop.fs.s3a.secret.key=abc"
+SPARK_TRAIN_ARGUMENTS="--conf spark.hadoop.fs.s3a.endpoint=s3.us-east-1.amazonaws.com --conf spark.hadoop.fs.s3a.access.key=<access_key> --conf spark.hadoop.fs.s3a.secret.key=<secret_key>"
 ML_ALGORITHM="dt" # rf for Random forest or dt for Decision Tree
 OUTPUT_METHOD="elasticsearch" #console or elasticsearch
 SPARK_TEST_ARGUMENTS="--conf spark.network.timeout=300 --conf spark.sql.streaming.metricsEnabled=true --conf spark.hadoop.fs.s3a.endpoint=s3.us-east-1.amazonaws.com
- --conf spark.hadoop.fs.s3a.access.key=Aqbq --conf spark.hadoop.fs.s3a.secret.key=acvb"
+ --conf spark.hadoop.fs.s3a.access.key=<access_key> --conf spark.hadoop.fs.s3a.secret.key=<secret_key>"
 ELASTICSEARCH_NODE_NAMES="node6.swarm1.network-intrusion-detect-pg0.emulab.net,node7.swarm1.network-intrusion-detect-pg0.emulab.net,nodees.swarm1.network-intrusion-detect-pg0.emulab.net"
 set +a
 
