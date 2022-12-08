3ba6914d7be98a0338a9666fe96d8bde160a7c33
Nguyễn Thành Thắng <76993858+zirami@users.noreply.github.com>
2022-12-08 19:43:45 +0700

    submit

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -a
 KAFKA_TOPIC="netflows"
-DATASET_LOCATION="../dataset/nsl-kdd"
-MODELS_LOCATION="../Models"
+DATASET_LOCATION="/home/sparkmaster/Desktop/thangnt7ir/Datasets/NSL-KDD"
+MODELS_LOCATION="/home/sparkmaster/Desktop/Models"
 TRAINING_FILE="KDDTrain+.txt"
 ML_ALGORITHM="rf" # rf cho Random forest hoac dt cho Decision Tree
 OUTPUT_METHOD="elasticsearch"
